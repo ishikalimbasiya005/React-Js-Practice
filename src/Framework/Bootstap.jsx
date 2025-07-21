dada941351142921fc21d3f087091410e047a52b
Ishika Limbasiya <ishikalimbasiya005@gmail.com>
2025-07-21 14:02:47 +0800

    Add New Framework in React.Js

---
+++ src/Framework/Bootstap.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Accordion from "react-bootstrap/Accordion";
+
+// Install Bootstrap command and then Run the code Using "npm start"
+// npm install react-bootstrap bootstrap
+
+const Bootstrap = () => {
+  return (
+    <>
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </>
+  );
+};
+
+export default Bootstrap;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// ++++++++++++++++++++++++++ frameworks ++++++++++++++++++++++++++
 // import Mui from './Framework/Mui.jsx';
-import AntDesign from './Framework/AntDesign.jsx';
+// import AntDesign from './Framework/AntDesign.jsx';
+import Bootstrap from './Framework/Bootstap.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     {/* <Mui /> */}
-    <AntDesign />
+    {/* <AntDesign /> */}
+    <Bootstrap />
   </>
 );
 
